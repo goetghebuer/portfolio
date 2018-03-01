26f8406b6ed162ba390b1d6a165a85c3582a6409
goetghebuer <patrick.goetghebuer@gmail.com>
2018-03-01 12:35:33 +0100

    header modifier

---
+++ public/js/app.js
@@ -0,0 +1,12 @@
+// Typejs - animate
+
+$('animate').typed({
+    strings: ["web developer", "UI/UX Designer", "Photography"],
+    loop: true,
+    startDelay: 1000,
+    backDelay: 1000,
+    typeSpeed: 30,
+    showCursor: true,
+    cursorChar: '|',
+    autoInsertCss: true
+});
